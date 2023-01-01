f60a60e8e2aed48a2387b137654d78d70ba905ed
Yashodha Dilshan <114228913+yashodhaDilshan@users.noreply.github.com>
2023-01-01 08:18:29 -0800

    Start

--- src/lk/ijse/fitnessCenter/AppInitializer.java
+++ src/lk/ijse/fitnessCenter/AppInitializer.java
@@ -19,7 +19,7 @@ public class  AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        Parent parent = FXMLLoader.load(this.getClass().getResource("view/MainMenu.fxml")); //LoginForm
+        Parent parent = FXMLLoader.load(this.getClass().getResource("view/Intro.fxml")); //LoginForm
         Scene scene = new Scene(parent);
         primaryStage.setScene(scene);
         primaryStage.centerOnScreen();
